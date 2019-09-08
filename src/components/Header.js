8b44c30a6d11ab4dbf04b70d35e4286b3f442529
arman <you@example.com>
2019-09-08 01:20:49 -0700

    change discription of arman

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ const Header = (props) => (
         <div className="content">
             <div className="inner">
                 <h1>arman</h1>
-                <p>A fully responsive site template designed by <a href="https://html5up.net">HTML5 UP</a> and released<br />
+                <p>HI my name is arman <a href="https://html5up.net">HTML5 UP</a> and released<br />
                 for free under the <a href="https://html5up.net/license">Creative Commons</a> license.</p>
             </div>
         </div>
